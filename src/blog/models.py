59fb2b0fea8da4763da543815b72baf44cc450aa
Jack Perry <jack.e.perry@outlook.com>
2022-12-24 11:19:30 -0700

    👌 IMPROVE: added categories

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,6 +16,10 @@ class Post(models.Model):
     blog_link = models.CharField(max_length=1_000, null=True)
     published_at = models.DateField(null=True)
     excerpt = models.CharField(max_length=100, null=True)
+    categories = models.ManyToManyField("Category", related_name="posts", null=True)
 
     def __str__(self):
         return f"{self.title} - {self.published_at}"
+
+    def categories_as_list(self):
+        return [x["name"] for x in self.categories.values()]
