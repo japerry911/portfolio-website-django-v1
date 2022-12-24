d3d0f8ed436751888327607292b62272c507c381
Jack Perry <jack.e.perry@outlook.com>
2022-12-24 09:01:03 -0700

    📦 NEW: added blog base routing and base page

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("", include("home.urls")),
     path("about", include("about.urls")),
     path("projects", include("projects.urls")),
+    path("blog", include("blog.urls")),
 ]
