2b168b9f211fc622f1a86cf5adc6ab0287ba7a65
Jack Perry <jack.e.perry@outlook.com>
2022-12-31 13:45:10 -0700

    🐛 FIX: fix ordering of skills

--- src/about/views.py
+++ src/about/views.py
@@ -5,7 +5,7 @@ from .models import Skill, TimelineEntry
 
 def index(request):
     timeline_entries = TimelineEntry.objects.all().order_by("order_number")
-    skills = Skill.objects.all().order_by("-title")
+    skills = Skill.objects.all().order_by("title")
 
     return render(
         request,
